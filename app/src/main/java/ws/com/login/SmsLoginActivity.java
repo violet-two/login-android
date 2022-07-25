2351e413cc6d08b34037aeb5c5539cac25625e53
violet-two <820918859@qq.com>
2022-07-25 17:57:57 +0800

    1

---
+++ app/src/main/java/ws/com/login/SmsLoginActivity.java
@@ -0,0 +1,14 @@
+package ws.com.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmsLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_login);
+    }
+}
