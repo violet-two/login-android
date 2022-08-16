a280121f36d7cdc5d144302705d1ffc2e4d3cd87
violet-two <820918859@qq.com>
2022-08-16 16:05:31 +0800

    修复了一些bug

--- login-ws-team/src/main/java/ws/com/login_ws_team/InformationDepartmentActivity.java
+++ login-ws-team/src/main/java/ws/com/login_ws_team/InformationDepartmentActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class InformationDepartmentActivity extends AppCompatActivity {
+
+    private TextView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_department);
+        search = findViewById(R.id.search);
+        search.setVisibility(View.GONE);
+
     }
 
     public void quit(View view) {
